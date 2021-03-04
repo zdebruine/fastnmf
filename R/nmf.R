4abf933337f346af1181c504e2e725105e547054
zdebruine <zacharydebruine@gmail.com>
2021-03-04 16:13:03 -0500

    Add files via upload

---
+++ R/nmf.R
@@ -0,0 +1,106 @@
+#' @title Non-negative matrix factorization
+#'
+#' @description NMF by alternating constrained least squares with fast solvers and an efficient 
+#' stopping criteria.
+#'
+#' @details
+#' NMF solves the equation \deqn{A = WH} by minimizing \eqn{A - WH}. In NMF by alternating least 
+#' squares, this objective is implicitly minimized and is thus never explicitly calculated. Rather,
+#' progress towards the objective is measured indirectly by the rate of change in \eqn{H} between 
+#' consecutive iterations.
+#'
+#' ## Model diagonalization
+#' By default, \code{amf::nmf} scales columns in \eqn{w} and rows in \eqn{h} to sum to 1 by 
+#' introducing a "scaling diagonal":
+#' \deqn{A = WDH}
+#' This is important in several ways:
+#' * **interpretability**: it is easy to see which features (or samples) are most important in a
+#' factor, not as a proportion of the total signal explained by the model, but as a proportion of 
+#' contribution to that specific factor/signal.
+#' * **regularization**: regularization is performed on the normalized matrices, thus regularization
+#' in a diagonalized model penalizes signals equally and not as a proportion of their relative
+#' contribution towards overall signal.
+#' * **multiple model alignment**: factorizations from multiple random starts rarely align.
+#' However, after sorting along the diagonal, they are much more likely to naturally align.
+#' 
+#' ## Model regularization
+#' Regularization, especially sparsifying regularization (i.e. L0, L1, PE) challenges the discovery
+#' of the best model solution. It may be most useful to first solve the unpenalized model, and then 
+#' add regularizations one at a time.
+#' 
+#' ## Exact solutions
+#' The challenges of exact nnls have been reviewed in \code{link{nnls}}. Exact NNLS is NP-hard 
+#' because it considers every possible feasible set.
+#'
+#' ## NNLS solvers used
+#' At present, only cold-start solvers are used (i.e. FAST, FAST + CD, nnls2, L0 = 1, and exact). 
+#' In the future, support will be added for warm-start solvers if they are faster.
+#'
+#' @param A matrix of features (rows) by samples (columns) to be factorized. Provide in dense 
+#' or sparse format.
+#' @param k rank of factorization
+#' @param min lower bound on solution, usually 0 (default). Vector of two for \code{c(w, h)}
+#' @param max upper bound on solution, if applicable (NULL, default). Vector of two for 
+#' \code{c(w, h)}.
+#' @param L0 cardinality of least squares solutions, if other than full-rank (NULL, default). If 1,
+#' an efficient exact solver is used. If 2 or greater, all possible feasible sets are sampled using 
+#' exact NNLS (NP-hard). Specify with caution, see details. Vector of two for \code{c(w, h)}
+#' @param L1 LASSO penalty to be subtracted from right-hand side of coefficients in least squares 
+#' updates, scaled to the maximum value in the right-hand side of each least squares problem. 
+#' Vector of two for \code{c(w, h)}.
+#' @param L2 Ridge regression penalty to be added to diagonal of \eqn{a}. Scaled to mean diagonal 
+#' value in coefficient matrix. Vector of two for \code{c(w, h)}.
+#' @param PE Pattern extraction penalty to be added to off-diagonal values in least squares 
+#' updates, scaled to mean diagonal value of the coefficient matrix. Vector of two for \code{c(w, h)
+#' }.
+#' @param exact calculate exact solution? See \code{\link{nnls}} for details. Use with caution. 
+#' Vector of two for \code{c(w, h)}.
+#' @param cd use coordinate descent to refine the solution? Vector of two for \code{c(w, h)}
+#' @param maxit maximum number of alternating updates of \eqn{w} and \eqn{h}
+#' @param maxit_cd in coordinate descent, maximum number of iterations
+#' @param tol relative change in \eqn{h} between consecutive iterations at convergence
+#' @param tol_cd in coordinate descent, distance of solution from maximum gradient residual at 
+#' convergence
+#' @param diag use a diagonal to scale columns in \eqn{w} and rows in \eqn{h} to sum to 1 (default 
+#' \code{TRUE})
+#' @param seed seed for random initialization of \eqn{w}
+#' @param threads use column-wise parallelization for each update of \eqn{w} and \eqn{h}. Specify 
+#' either 0 (all available threads, default) 1 (no parallelization), or any valid number.
+#' @param verbose show tolerance for each iteration
+#' @param ... additional parameters
+#' @returns A list of \code{w}, \code{d}, and \code{h} giving the factorization result, and 
+#' \code{iter} giving the number of iterations required to converge, and \code{tol} giving the 
+#' relative change in \eqn{h} between the last two iterations.
+#' @export
+#' @seealso \code{\link{nnls}}
+#' @md
+#' @import Matrix RcppArmadillo
+#'
+nmf <- function(A, k, min = c(0, 0), max = c(NULL, NULL), L0 = c(NULL, NULL), L1 = c(0, 0),
+    L2 = c(0, 0), PE = c(0, 0), exact = c(FALSE, FALSE), cd = c(TRUE, TRUE), maxit = 100,
+    maxit_cd = 100, tol = 0.01, tol_cd = 1e-8, diag = TRUE, seed = NULL, threads = 0,
+    verbose = TRUE, ...) {
+
+  if (is.null(seed)) seed <- 0
+  if (is.null(L0[1])) L0[1] <- k
+  if (is.null(L0[2])) L0[2] <- k
+  if (is.null(max)) max <- c(min[1], min[2])
+  A <- as(A, "dgCMatrix")
+  if (any(exact == TRUE) || any(L0 != k)) {
+    check_exact <- is.null(list(...)$check_exact)
+    if (check_exact) check_sanity(k, L0[1])
+    if (check_exact) check_sanity(k, L0[2])
+  }
+  return(c_nmf(A, k, min[1], min[2], max[1], max[2], maxit_cd, tol_cd, L0[1], L0[2], L1[1], L1[2],
+  L2[1], L2[2], PE[1], PE[2], exact[1], exact[2], cd[1], cd[2], maxit, tol, threads, diag, seed,
+  verbose))
+}
+
+check_sanity <- function(n, L0) {
+  msg <- "You have requested an exact solution in a very large feasible set search space. 
+  Please be sure you wish to proceed. If so, specify \"check_exact = FALSE\" in your function call."
+  if (L0 != n) {
+    if ((L0 > 10 && n > 15) || (L0 > 7 && n > 50) || (L0 > 5 && n > 100) || (L0 > 3 && n > 200) ||
+      (L0 > 1 && n > 500)) stop(msg)
+  } else if (n > 15) stop(msg)
+}
---
+++ R/nnls.R
@@ -0,0 +1,130 @@
+#' @title Non-negative least squares
+#'
+#' @description Solve a system of equations with non-negative constrained least squares. 
+#'
+#' @details
+#' Solve the equation \eqn{ax = b} for \eqn{x} when \eqn{a} is symmetric positive definite.
+#' Solves the equation \eqn{aa^Tx = ab_j} for all columns \eqn{j} in \eqn{b} when \eqn{a} 
+#' is rectangular.
+#'
+#' ## Adaptive Solvers
+#' \code{nnls} is an adaptive solver with several subroutines that may be called based on the
+#' requirements of the solution. The entire solver is written in C++ using the Armadillo
+#' library with OpenMP parallelization across columns in \eqn{b} (if \eqn{b} is a matrix). 
+#' 
+#' * **fast approximate solution trajectory** (FAST) non-negative least squares. 
+#' Used to initialize coordinate descent if no initial \code{x_init} is provided, but also gives
+#' a near-exact solution in well-conditioned systems with excellent speed. Set \code{cd = FALSE} 
+#' to use only the FAST solver.
+#' * **coordinate descent** (cd) finds the best solution discoverable by convex optimization 
+#' given an initial start (either \eqn{x_init} or the result from FAST). The coordinate descent
+#' solver also handles bounded variable constraints (\code{min}, \code{max}), and multinomial 
+#' constraints (\code{values}). Coordinate descent may be disabled by setting \code{cd = FALSE} 
+#' to use FAST only, but will only be disabled if \code{min = 0, max = NULL, values = NULL}.
+#' * **singular L0**. Exact solutions with singular cardinality are found efficiently when L0 = 1
+#' * **2-variable solutions** are found exactly by hard-coded inversion and substitution.
+#' * **exact nnls**: NNLS is often not a perfectly convex problem, and thus the global solution
+#' minimum is often not discoverable by convex optimization. Exact NNLS finds the true solution
+#' by solving the solution for every possible feasible set. Note that the number of possible
+#' solutions increases exponentially with the number of variables in the system (NP-hard). Exact 
+#' NNLS is only reasonable for problems of up to ~15 variables or for correspondingly small L0 
+#' cardinalities. Exact NNLS is the only supported method for finding L0 solutions, as methods for 
+#' approximating L0 are very inaccurate.
+#'
+#' ## Input Requirements
+#' There are two formats for input matrices. The first corresponds to the equation:
+#' 
+#' \deqn{ax = b}
+#' 
+#' where \eqn{a} is symmetric positive definite \emph{k x k}, and \emph{x} and \emph{b} are vectors
+#' of length \emph{k} or equidimensional matrices with \emph{k} rows.
+#' 
+#' The second format corresponds to the classical alternating matrix factorization update problem:
+#' 
+#' \deqn{A = WH}
+#' 
+#' When updating \eqn{H}, we have:
+#' 
+#' \deqn{W^TWH = WA_j}
+#' \deqn{a = W^TW}
+#' \deqn{x = H}
+#' \deqn{b = WA_j}
+#'
+#' for all columns \eqn{j} in \eqn{A}. 
+#' In \code{nnls}, \eqn{A} may be specified as \eqn{b}, \eqn{W^T} as \eqn{a}, and \eqn{H} as 
+#' \eqn{x_init}. Note that when \code{nnls} checks whether \eqn{W} is symmetric positive definite, it 
+#' pivots to the above interpretation of parameters.
+#' 
+#' In the case above, \eqn{a} is of dimensions \emph{k x m}, \eqn{b} is of dimensions \emph{m x n}, 
+#' and \eqn{x} is of dimensions {k x n}. \code{nnls} will try to rearrange \eqn{a}, \eqn{b}, and 
+#' \eqn{x} to align along common edges if improper dimensions are provided.
+#' 
+#' The corresponding equation for updating \eqn{W} in block-pivoting is:
+#' 
+#' \deqn{HH^TW^T = HA^T_j}
+#'
+#' @param a symmetric positive definite \emph{k x k} matrix or rectangular \emph{k x m} matrix 
+#' giving the coefficients of the linear system.
+#' @param b vector or dense/sparse matrix giving right-hand side(s) of the linear system. If \eqn{a}
+#' is symmetric positive definite, \eqn{b} must be a positive vector of length {k}. If \eqn{a} is
+#' of dimensions \emph{k x m}, \emph{b} must be a dense/sparse matrix of dimensions \emph{m x n} or 
+#' a vector of length \emph{m}.
+#' @param x_init if provided, initial value for x. If \eqn{a} is symmetric positive definite, 
+#' \eqn{x} must be of the same dimensions as \eqn{b}. If \eqn{a} is rectangular, \eqn{x} must be a 
+#' matrix of dimensions \emph{k x n}. If a non-zero \eqn{x_init} is provided, coordinate descent 
+#' will refine the solution immediately without initialization by FAST.
+#' @param values multinomial array (including 0) to which variables in the solution will be 
+#' constrained, if applicable (NULL, default)
+#' @param min lower bound on solution, usually 0 (default)
+#' @param max upper bound on solution, if applicable (NULL, default)
+#' @param L0 cardinality of the solution, if other than full-rank (NULL, default). If 1, an 
+#' efficient exact solver is used. If 2 or greater, all possible feasible sets are sampled using 
+#' exact NNLS (NP-hard). Specify with caution, see section on exact NMF in details.
+#' @param L1 LASSO penalty to be subtracted from \code{b}. Scaled to maximum value in each column 
+#' of \code{b}.
+#' @param L2 Ridge regression penalty to be added to diagonal of \code{a}. Scaled to mean diagonal 
+#' value in \code{a}.
+#' @param PE Pattern Extraction penalty to be added to off-diagonal values in \code{a}. Scaled 
+#' to mean diagonal value in \code{a}.
+#' @param threads If \code{b} is a matrix, apply column-wise parallelization by specifying either 0 
+#' (all available threads, default) or a number other than 1.
+#' @param maxit in coordinate descent, maximum number of iterations
+#' @param tol in coordinate descent, distance of solution from maximum gradient residual at 
+#' convergence
+#' @param exact find an exact solution (default FALSE). Use with caution. Extremely slow and 
+#' possibly intractable. See details. This is the only method for \code{L0} solving.
+#' @param cd Should coordinate descent be used to refine the initial solution approximated by FAST?
+#' By default this is true, but for well-conditioned systems FAST often gives a very good solution
+#' and cd can be disabled for speed gains. See details.
+#' @param ... additional parameters
+#' @returns \eqn{x}, a vector or matrix containing constrained solutions to the linear system(s)
+#' @export
+#' @md
+#' @import Matrix RcppArmadillo
+#' @examples
+#' A <- matrix(runif(25*10), 25, 10)
+#' B <- matrix(runif(25*1000)*0.1, 25, 1000)
+#' X <- nnls(A, B)
+#'
+nnls <- function(a, b, x_init = NULL, min = 0, max = NULL, values = NULL, L0 = NULL, L1 = 0,
+    L2 = 0, PE = 0, threads = 0, maxit = 100, tol = 1e-8, exact = FALSE, cd = TRUE, ...) {
+
+  check_exact <- is.null(list(...)$check_exact)
+  if (is.null(max)) max <- min
+  if (is.null(values)) values <- c(0)
+  if (is.null(x_init)) x_init <- matrix(0, 1, 1)
+  if ("numeric" %in% class(b)) b <- as.matrix(b)
+  if (!("dgcMatrix" %in% class(b))) b <- as(b, "dgCMatrix")
+  if (is.null(L0)) L0 = 0
+  if (check_exact && L0 != 0) check_sanity(ncol(A), L0)
+  return(c_nnls(a, b, x_init, values, threads, min, max, maxit, tol, L0, L1, L2, PE, exact, cd))
+}
+
+check_sanity <- function(n, L0) {
+  msg <- "You have requested an exact solution in a very large feasible set search space. 
+  Please be sure you wish to proceed. If so, specify \"check_exact = FALSE\" in your function call."
+  if (L0 != n) {
+    if ((L0 > 10 && n > 15) || (L0 > 7 && n > 50) || (L0 > 5 && n > 100) || (L0 > 3 && n > 200) ||
+      (L0 > 1 && n > 500)) stop(msg)
+  } else if (n > 15) stop(msg)
+}
